218696cc41c30bd5de4ed7c44b3067539d35021d
mxkmn <mxkmnz@gmail.com>
2021-11-24 01:58:08 +0800

    ds

--- projects/scanwalk_qr_generator/main.js
+++ projects/scanwalk_qr_generator/main.js
@@ -27,3 +27,15 @@ if ('serviceWorker' in navigator) {
 	.register('pwa_sw.js')
 	.then(() => { console.log('Service Worker Registered'); });
 }
+
+const isIos = () => {
+	const userAgent = window.navigator.userAgent.toLowerCase();
+	return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+	this.setState({ showInstallMessage: true });
+}
