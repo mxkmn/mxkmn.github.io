b8da1f8e55f2b44b4176db13158abd95c26be5e5
mxkmn <mxkmnz@gmail.com>
2022-12-24 15:03:23 +0800

    Странно, что этот коммит не был создан

--- projects/scanwalk_qr_generator/index.js
+++
@@ -1,9 +0,0 @@
-// очистка данных сайта
-localStorage.clear();
-
-function activate(el) {
-    alert("Ошибка подключения, попробуйте подождать и перезагрузить страницу");
-}
-
-// получение токена
-communications.getTokens();
